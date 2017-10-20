44d1da692051e37cf2fb76142987cf19e0acb0fd
DavidDwyer87 <David_Dwyer@live.com>
2017-10-20 11:11:39 -0500

    remove document ready function

--- Pixel Art Maker/design.js
+++ Pixel Art Maker/design.js
@@ -1,39 +1,39 @@
-$(document).ready(function(){
-	$('#btnSubmit').click(function(){
+$('#btnSubmit').click(function(){
 		makeGrid();
 	});
 
-	function makeGrid()
-	{
-		try
-		{			
-			let rows = $('#gridHeight').val();
-			let columns = $('#gridWidth').val();
+function makeGrid()
+{
+	try
+	{	
+	    //get values from the textboxs		
+		let rows = $('#gridHeight').val();
+		let columns = $('#gridWidth').val();
 
-			let table = $('#canvas_table');
-			
-			//clear table before adding new rows and columns
-			table.empty();
-
-			//rows
-			for(let i=0; i<rows; i++)
-			{
-				table.append("<tr id=r"+i+"></tr>");
-				//columns
-				for(let j=0; j<columns; j++)
-				{
-					$('#r'+i).append("<td id='c"+i+"_"+j+"'></td>");
-				}
-			}
-
-			$('td').on('click',function(){
-				console.log($(this).css('background-color',$('#colorPicker').val()));
-			});
-		}
-		catch(err)
-		{
-			alert(err);
-		}
+		let table = $('#canvas_table');
 		
-	}		
-});
+		//clear table before adding new rows and columns
+		table.empty();
+
+		//rows
+		for(let i=0; i<rows; i++)
+		{
+			table.append("<tr id=r"+i+"></tr>");
+			//columns
+			for(let j=0; j<columns; j++)
+			{
+				$('#r'+i).append("<td id='c"+i+"_"+j+"'></td>");
+			}
+		}
+
+        //click event for each cell in the table
+		$('td').on('click',function(){
+			console.log($(this).css('background-color',$('#colorPicker').val()));
+		});
+	}
+	catch(err)
+	{
+		alert(err);
+	}
+	
+}
